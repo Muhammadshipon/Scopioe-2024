888d35b313df5d860f73fa85b3a6c3a84cb44271
Muhammad <shalauddinahmedshipon@gmail.com>
2024-08-02 17:51:05 -0800

    Project setup is done

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import AuthProvider from './provider/AuthProvider';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthProvider>
+    <RouterProvider router={router} />
+    </AuthProvider>
+   </React.StrictMode>,
+)
